7ab2f4e9dd7da1a99176a6e1e7bdc8d6c135eb0b
JaquelineFCRodrigues <j.g.rodrigues@accenture.com>
2023-03-30 16:54:48 -0300

    feat: Add MUI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 // Configuração do React Router Dom
 export const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/pagina-inicial" element= {<p>Página Inicial</p>} />
+            <Route path="/pagina-inicial" element= {<Button>Teste</Button>} />
             <Route path="*" element= {<Navigate to="/pagina-inicial" />} /> 
         </Routes>
     );
